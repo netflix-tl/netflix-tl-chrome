c0fcb2d19d9fdb97825b92d7f5c4679269e69725
bkucera3 <benjamin.kucera.stu@gtri.gatech.edu>
2017-07-03 18:27:48 -0400

    Refactor popup to have main module

---
+++ src/popup/main/main.js
@@ -0,0 +1,73 @@
+import Options from '../options/options'
+import MainDOM from './main.dom'
+import * as User from '../../model/User'
+import * as Comment from '../../model/Comment'
+import * as Group from '../../model/Group'
+
+function initApp() {
+
+  let DOM = new MainDOM()
+
+  /**
+   * closeLoginPrompt and loginPrompt are passed to User.js,
+   *   and are called when the login/logout status of the 
+   *   user changes.
+   */
+  User.initializeFirebase(closeLoginPrompt, loginPrompt)
+
+  DOM.submitBtn.addEventListener('click', submit)
+  DOM.optionsBtn.addEventListener('click', openOptions)
+  DOM.logout.addEventListener('click', logout)
+  DOM.inputField.addEventListener('keydown', inputKeydown)
+
+
+  function submit() {
+    Comment.postComment(DOM.inputField.value)
+    DOM.clearInputField()
+  }
+
+  function openOptions() {
+    Options()
+  }
+
+  function logout() {
+    User.logout()
+  }
+
+  function inputKeydown(e) {
+    if (e.keyCode === 13) {
+      DOM.submitBtn.click()
+    }
+    DOM.updateCharLabel()
+  }
+
+  function loginPrompt() {
+    if (!User.isUserLoggedIn()) {
+      let el = document.createElement('div')
+      el.innerHTML = DOM.getLoginOverlay();
+      document.body.appendChild(el)
+      setTimeout(() => document.getElementsByClassName('login-overlay')[0].classList.remove('loading'), 200)
+      document.getElementById('welcome-login-btn').addEventListener('click', () => User.login())
+    }
+  }
+
+  function closeLoginPrompt() {
+    let overlay = document.getElementsByClassName('login-overlay')[0];
+    if (!overlay) { return console.log('prompt not open') }
+    overlay.classList.add('loading')
+    setTimeout(() => overlay.parentElement.remove(), 500)
+  }
+
+  /**
+ * This function should be extracted out to wherever
+ *  the group functionality is made. ../groupview/groupview.js ?
+ */
+  function createGroup() {
+    Group.createGroup({ name: DOM.inputField.value })
+    DOM.clearInputField()
+  }
+}
+
+window.onload = function () {
+  initApp()
+}
--- src/popup/options/options.js
+++ src/popup/options/options.js
@@ -1,14 +1,5 @@
-import html from './options.html'
-import './options.css'
+import OptionsDOM from "./options.dom"
 
 export default function Options() {
-    document.getElementsByClassName('options-section')[0].innerHTML = html
-    document.getElementsByClassName('main-section')[0].classList.add('left')
-    document.getElementsByClassName('options-section')[0].classList.add('left');
-    document.getElementsByClassName('back-btn')[0].addEventListener('click', () => {
-        console.log('back')
-        document.getElementsByClassName('main-section')[0].classList.remove('left')
-        document.getElementsByClassName('options-section')[0].classList.remove('left')
-        
-    })
+    let DOM = new OptionsDOM()
 }
