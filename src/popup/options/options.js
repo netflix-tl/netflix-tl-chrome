a68a1e850fa8ee9d916450c89e01da38388d7f61
Ben Kucera <Bkucera@users.noreply.github.com>
2017-07-06 22:08:47 -0400

    #37: Use Materialize, Sass (#38)

--- src/popup/options/options.js
+++ src/popup/options/options.js
@@ -2,4 +2,24 @@ import OptionsDOM from "./options.dom"
 
 export default function Options() {
     let DOM = new OptionsDOM()
+    let options = {
+        notifications: true //TODO: pull this from a settings file
+    }
+
+    initSettings()
+
+
+    function initSettings() {
+        DOM.toggleNotifications(options.notifications)
+        DOM.notificationToggle.addEventListener('click', toggleNotifications)
+    }
+    function toggleNotifications() {
+        if (!options.notifications) {
+            options.notifications = true
+            DOM.toggleNotifications(true)
+        } else {
+            options.notifications = false
+            DOM.toggleNotifications(false)
+        }
+    }
 }
